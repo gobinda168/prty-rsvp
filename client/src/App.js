82e256fc909d0c4661cbfceeebea749592d12907
Gobinda Das <gobinda168@gmail.com>
2019-10-02 15:20:17 +0530

    Complete static UI

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/layouts/Navbar'
+import Home from './components/pages/Home'
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
